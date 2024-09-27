c7e58ad7b10325d1eb8c619194aadf41b882082a
DiegodelosV <diegodelosv@gmail.com>
2024-09-27 12:32:46 -0300

    crea el archivo auth.js con la configuracion de Firebase para autenticación y componente SignUpView.vue para el registro del usuario

---
+++ src/views/SignUpView.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="register">
+    <h1>Registro</h1>
+    <input type="text" placeholder="Correo Electronico" v-model="email" />
+    <input type="password" placeholder="Contraseña" v-model="password" />
+    <button type="submit">Registrarse</button>
+  </form>
+</template>
+
+<script>
+import { createUserWithEmailAndPassword } from "../auth.js";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async register() {
+      try {
+        const userCredential = await createUserWithEmailAndPassword(
+          this.email,
+          this.password
+        );
+        console.log(userCredential, "usuario creado exitosamente");
+      } catch (error) {
+        console.log(error.code, error.message);
+      }
+    },
+  },
+};
+</script>
